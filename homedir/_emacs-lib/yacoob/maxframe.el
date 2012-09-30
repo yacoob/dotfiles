a543b6b06254c0e6d5ebdf693135f0b638dc6673
Jakub Turski <yacoob@gmail.com>
2012-10-01 00:18:33 +0100

    Remove Emacs config.

--- homedir/_emacs-lib/yacoob/maxframe.el
+++
@@ -1,8 +0,0 @@
-;; frame maximizer
-
-;; - biggest width to maximize to (needed with >1 display)
-(setq mf-max-width 1920)
-;; - maximize on startup
-(add-hook 'window-setup-hook 'maximize-frame t)
-;; - keybind to maximize Emacs
-;(global-set-key [(meta return)] 'mf)
