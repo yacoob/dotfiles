f8b98a0e1095884c88e94759dbc38a897e7262b5
Jakub Turski <yacoob@gmail.com>
2014-03-09 02:25:52 +0000

    (osx) remove features of keyremap4macbook that I don't use.

--- initial/keyremap4macbook.sh
+++ initial/keyremap4macbook.sh
@@ -2,15 +2,11 @@
 
 cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
 
-$cli set remap.pointing_relative_mousebutton4_to_scroll 1
+$cli set repeat.wait 23
 /bin/echo -n .
-$cli set remap.simultaneouskeypresses_expose_kl 1
+$cli set repeat.initial_wait 300
 /bin/echo -n .
 $cli set remap.pointing_leftrightclick2expose 1
-/bin/echo -n .
-$cli set remap.controlL2controlL_escape 1
-/bin/echo -n .
-$cli set remap.vimode_toggle_commandLR 1
 /bin/echo -n .
 $cli set remap.iso_swap_tilda_section 1
 /bin/echo -n .
