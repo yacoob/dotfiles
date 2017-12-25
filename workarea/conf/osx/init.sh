3f39814036d137f777cafc3c887d428aa49e3c64
Jakub Turski <yacoob@gmail.com>
2017-12-25 23:12:03 +0000

    Consolidate osx specific files, update osx init file.

---
+++ workarea/conf/osx/init.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#set -x
+source ~/.yacoob-conf
+BASEDIR=${CONFDIR}/osx
+
+# Main screen turn on!
+if [[ "$1" == "-n" ]]; then
+    DEBUG="echo "
+fi
+
+# Install homebrew packages.
+BREW_PREFIX=~/brew
+BREW_LIST_FILE=${BASEDIR}/brew.list
+CASK_LIST_FILE=${BASEDIR}/cask.list
+BREW=${BREW_PREFIX}/bin/brew
+if [[ ! -x "${BREW}" ]]; then
+  ${DEBUG} mkdir -p ${BREW_PREFIX}
+  cd ${BREW_PREFIX}
+  ${DEBUG} curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+fi
+${DEBUG} cd -
+${DEBUG} ${BREW} analytics off
+${DEBUG} ${BREW} install $(cat ${BREW_LIST_FILE}) $(cat ${BREW_LIST_FILE}.${LOCATION} 2>/dev/null )
+${DEBUG} ${BREW} tap caskroom/cask
+${DEBUG} ${BREW} tap caskroom/fonts
+${DEBUG} ${BREW} cask install $(cat ${CASK_LIST_FILE} ${CASK_LIST_FILE}.${LOCATION} 2>/dev/null )
+${DEBUG} ${BREW} cleanup -s
+ZSH=${BREW_PREFIX}/bin/zsh
+grep -q "${ZSH}" /etc/shells
+if [[ $? -ne 0 ]]; then
+    echo "${ZSH}" | sudo tee -a /etc/shells
+fi
+
+# Apply OSX tweaks.
+while read line; do
+  ${DEBUG} defaults write ${line}
+done < ${BASEDIR}/tweaks
+
+# Make sure ~/.yacoob-conf is available for gui apps (ie. MacVim)
+grep -q '.yacoob-conf' /etc/zshenv
+if [[ $? -ne 0 ]]; then
+  ${DEBUG} echo 'source ~/.yacoob-conf' | sudo tee -a /etc/zshenv
+fi
+
+# Link iTerm's remotes
+${DEBUG} ln -sf "${BASEDIR}/iterm.remotes" "${HOME}/Library/Application Support/iTerm2/DynamicProfiles/remotes"
+
+# Drop in plists.
+for file in ${BASEDIR}/plists/*.plist; do
+  ${DEBUG} cp -f ${file} ${HOME}/Library/Preferences/${file##*/}
+done
