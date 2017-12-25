3929ddfdd92176bc6b9ef617a40d78d107652d03
Jakub Turski <yacoob@gmail.com>
2017-12-25 21:18:24 +0000

    Adjust init script to new layout.

--- workarea/conf/init.sh
+++ workarea/conf/init.sh
@@ -1,22 +1,36 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 shopt -s extglob
 
-print_help_and_exit() {
-    echo "  Usage: $0 <OS> <LOCATION>"
-    echo "example: $0 osx home"
-    exit 0
-}
-
-if  [[ $# -ne 2 ]] \
- || [[ "$1" != +([[:alpha:]]) ]] \
- || [[ "$2" != +([[:alpha:]]) ]]; then
-    print_help_and_exit;
+# check usage
+if  [[ $# -ne 2 ]]; then
+  echo "  Usage: $0 <OS> <LOCATION>"
+  echo "example: $0 osx home"
+  exit 1
 fi
 
-FILE=${HOME}/.yacoob-conf
-rm -f ${FILE}
+# helper function to operate on conf repository
+dotrepo() { git --git-dir=${TARGET} --work-tree=${HOME} $@}
 
-CONFDIR=$(pwd)
+# pull in the repo
+TARGET=${HOME}/.dotrepo
+BRANCH=new-layout
+git clone --bare -b ${BRANCH} https://github.com/yacoob/conf ${TARGET}
+dotrepo config status.showUntrackedFiles no
+dotrepo config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
+dotrepo fetch
+dotrepo checkout ${BRANCH}
+if [ $? != 0 ]; then
+  BKP=~/dotrepo-backup
+  mkdir ${BKP}
+  files=($(dotrepo checkout 2>&1 | egrep '\s+\.'))
+  foreach f ($files) { mv ${f} ${BKP}/ }
+  dotrepo checkout -b host-$(hostname) --track origin/${BRANCH}
+fi
+
+# create local meta config
+FILE=${HOME}/.yacoob-conf
+CONFDIR=${TARGET}
+rm -f ${FILE}
 echo "export OS=$1" > ${FILE}
 echo "export LOCATION=$2" >> ${FILE}
 echo "export CONFDIR=${CONFDIR}" >> ${FILE}
