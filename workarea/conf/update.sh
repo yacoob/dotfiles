f6cfe2ae14d24e40f2385bba39412b2137b5bdd6
Jakub Turski <yacoob@gmail.com>
2017-12-25 22:33:57 +0000

    Revert update.sh back to bash style.

--- workarea/conf/update.sh
+++ workarea/conf/update.sh
@@ -1,4 +1,7 @@
-#!/usr/bin/env zsh
+j!/usr/bin/env bash
+confgit() {
+  git --git-dir=${CONFREPODIR} --work-tree=${HOME} $@
+}
 source ~/.yacoob-conf
 confgit remote update origin > /dev/null 2>&1 || exit 1
 reslog=$(confgit log ${CONFBRANCH}..origin/${CONFBRANCH} --oneline)
