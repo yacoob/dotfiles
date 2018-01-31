ffdf07a2a6505215d9fe09af10d7166c370c6375
Jakub Turski <yacoob@gmail.com>
2018-01-31 16:33:42 +0000

    Grrr.

--- workarea/conf/osx/init.sh
+++ workarea/conf/osx/init.sh
@@ -46,7 +46,7 @@ fi
 # Link iTerm's remotes
 d="${HOME}/Library/Application Support/iTerm2/DynamicProfiles"
 [[ -d "$d" ]] || mkdir -p "${d}"
-${DEBUG} ln -sf "${BASEDIR}/iterm.remotes" "${HOME}/Library/Application Support/iTerm2/DynamicProfiles/remotes"
+${DEBUG} ln -sf "${BASEDIR}/iterm.remotes" "${d}/remotes"
 
 # Drop in plists.
 for file in ${BASEDIR}/plists/*.plist; do
