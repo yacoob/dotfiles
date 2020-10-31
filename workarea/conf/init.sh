5d82135447f32bbe11ece39df43379710b16e5b0
Jakub Turski <yacoob@gmail.com>
2020-10-31 21:18:16 +0000

    Allow the init script to clone from a custom location.
    I can now override the source of the `git clone` command. Once cloned,
    the URLs in confgit are normalized - pull from Github via https (for
    cronjob), push to Github via ssh. This is going to be useful for the
    docker build process, where I'll pull from a local directory populated
    from the build context.
    
    Also, change the last conditional bit to a proper `if` sequence, so it
    doesn't mutilate the exit status.

--- workarea/conf/init.sh
+++ workarea/conf/init.sh
@@ -2,6 +2,9 @@
 OS=${OS:-linux}
 LOCATION=${LOCATION:-home}
 TARGET=${HOME}/.dotrepo
+DEFAULT_PULL_URL="https://github.com/yacoob/conf"
+REPO_PULL_URL=${REPO_PULL_URL:-${DEFAULT_PULL_URL}}
+REPO_PUSH_URL=${REPO_PUSH_URL:-git@github.com:yacoob/conf.git}
 BRANCH=master
 METAFILE=${HOME}/.yacoob-conf
 CONFDIR=${HOME}/workarea/conf
@@ -12,12 +15,17 @@ confgit() {
 }
 
 # pull in the repo
-git clone --bare -b ${BRANCH} https://github.com/yacoob/conf ${TARGET}
+git clone --bare -b ${BRANCH} ${REPO_PULL_URL} ${TARGET}
 confgit config status.showUntrackedFiles no
 confgit config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
 confgit fetch
 confgit checkout -f ${BRANCH}
 confgit branch --set-upstream-to=origin/${BRANCH} ${BRANCH}
+# Normalize the remote URLs regardless of where we've cloned from:
+# - we should always pull from Github over https
+# - we should always push to Github over ssh
+confgit remote set-url origin --push ${REPO_PUSH_URL}
+confgit remote set-url origin ${DEFAULT_PULL_URL}
 
 # set up meta config
 rm -f ${METAFILE}
@@ -32,7 +40,6 @@ cat ${METAFILE}
 
 # run OS-specific script if there is one
 s=${CONFDIR}/${OS}/init.sh
-[[ -x $s ]] && echo "Now run $s by hand."
-
-# Make sure the exit code is 0.
-:
+if [[ -x $s ]]; then
+  echo "Now run $s by hand."
+fi
