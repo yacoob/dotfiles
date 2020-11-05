cdc86d5112333aa5b359f43e928426b22aae32f6
Jakub Turski <yacoob@gmail.com>
2020-11-05 00:16:05 +0000

    Use current branch name during bootstrap.
    Once I switch from path to git context I might have to change this to
    use `GITHUB_REF`.

--- workarea/conf/init.sh
+++ workarea/conf/init.sh
@@ -5,7 +5,7 @@ TARGET=${HOME}/.dotrepo
 DEFAULT_PULL_URL="https://github.com/yacoob/conf"
 REPO_PULL_URL=${REPO_PULL_URL:-${DEFAULT_PULL_URL}}
 REPO_PUSH_URL=${REPO_PUSH_URL:-git@github.com:yacoob/conf.git}
-BRANCH=master
+BRANCH=${BRANCH:-master}
 METAFILE=${HOME}/.yacoob-conf
 CONFDIR=${HOME}/workarea/conf
 
@@ -15,6 +15,15 @@ confgit() {
 }
 
 # pull in the repo
+#
+# Look, I know `set -e` is not the best idea as the shell will pout and exit on
+# any non-zero exit status. This entire section is all single command git calls
+# though, and if any of them fails it leaves the target dir in possibly weird
+# state. In a docker build process this also results in *next* build steps
+# failing in a weird manner. So I want this block below to succed fsvo
+# 'success' but sure as hell I'm not going to write error checking for every
+# single command.
+set -e
 git clone --depth 1 --bare -b ${BRANCH} ${REPO_PULL_URL} ${TARGET}
 confgit config status.showUntrackedFiles no
 confgit config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
@@ -26,6 +35,7 @@ confgit branch --set-upstream-to=origin/${BRANCH} ${BRANCH}
 # - we should always push to Github over ssh
 confgit remote set-url origin --push ${REPO_PUSH_URL}
 confgit remote set-url origin ${DEFAULT_PULL_URL}
+set +e
 
 # set up meta config
 rm -f ${METAFILE}
