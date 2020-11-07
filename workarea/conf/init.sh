3e2b9f16b2ac5efdbd691777178ac6163cc65cfb
yacoob <yacoob@gmail.com>
2020-11-07 02:36:38 +0000

    Redo github actions
    - It now triggers on pull_request and push to any branch
    - On pull_request: build image as a sanity check.
    - On push: build image and push to registry. Tag as
      `latest` only if it was a push to the main branch.
    - On succesful build and push on master, ping `yacoob/gcu` to rebuid.
    - `init.sh` can now set things up based on a custom branch or even 
      a detached head repo (eg. checked out random commit)

--- workarea/conf/init.sh
+++ workarea/conf/init.sh
@@ -5,7 +5,8 @@ TARGET=${HOME}/.dotrepo
 DEFAULT_PULL_URL="https://github.com/yacoob/conf"
 REPO_PULL_URL=${REPO_PULL_URL:-${DEFAULT_PULL_URL}}
 REPO_PUSH_URL=${REPO_PUSH_URL:-git@github.com:yacoob/conf.git}
-BRANCH=master
+BRANCH=${BRANCH:-master}
+REPO_IS_DETACHED_HEAD=${REPO_IS_DETACHED_HEAD:-false}
 METAFILE=${HOME}/.yacoob-conf
 CONFDIR=${HOME}/workarea/conf
 
@@ -15,17 +16,44 @@ confgit() {
 }
 
 # pull in the repo
-git clone --depth 1 --bare -b ${BRANCH} ${REPO_PULL_URL} ${TARGET}
+#
+# Look, I know `set -e` is not the best idea as the shell will pout and exit on
+# any non-zero exit status. This entire section is all single command git calls
+# though, and if any of them fails it leaves the target dir in possibly weird
+# state. In a docker build process this also results in *next* build steps
+# failing in a weird manner. So I want this block below to succed fsvo
+# 'success' but sure as hell I'm not going to write error checking for every
+# single command.
+echo "Trying to clone the config files."
+echo "  from: ${REPO_PULL_URL}"
+echo "  branch: ${BRANCH}"
+# Are we cloning from a normal repository or a detached HEAD one?
+# The latter shows up in Github's CI during pull requests.
+if [[ "${REPO_IS_DETACHED}" = "false" ]]; then
+  # Normal repository.
+  set -e
+  echo "I'm assuming the repo contains the branch I'd like to pull."
+  # Fetch latest revision of specified branch.
+  git clone --depth 1 --bare -b ${BRANCH} ${REPO_PULL_URL} ${TARGET}
+  # Populate the work tree.
+  confgit checkout
+ else
+  set -e
+  echo "I'm assuming the remote repo has a detached HEAD and not the branch I'd like to pull."
+  # Fetch latest revision; there's no information about which branch is it
+  # though.
+  git clone --depth 1 --bare ${REPO_PULL_URL} ${TARGET}
+  # ...so we can create a matching branch locally.
+  confgit checkout -B ${BRANCH}
+fi
+# Don't complain about other files in ~ that are not part of this repo.
 confgit config status.showUntrackedFiles no
-confgit config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
-confgit fetch
-confgit checkout -f ${BRANCH}
-confgit branch --set-upstream-to=origin/${BRANCH} ${BRANCH}
 # Normalize the remote URLs regardless of where we've cloned from:
 # - we should always pull from Github over https
 # - we should always push to Github over ssh
 confgit remote set-url origin --push ${REPO_PUSH_URL}
 confgit remote set-url origin ${DEFAULT_PULL_URL}
+set +e
 
 # set up meta config
 rm -f ${METAFILE}
