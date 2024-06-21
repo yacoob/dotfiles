e6230997180d2b79bc2764e313e47552c90fb1c0
Jakub Turski <yacoob@ftml.net>
2024-06-21 18:42:51 +0100

    (nvim) Migrate vim settings
    - don't migrate stuff that has a sensible default in nvim
    - don't use nvim's api (eg. vim.opt, vim.g, vim.cmd.nvim_create_autocmd
      for the moment, as the vim names and commands are more familiar now

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,99 @@
--- install lazy
+-- nvim settings
+vim.cmd([[
+" whitespace
+set expandtab                   " use spaces, not tabs
+set list                        " highlight some special characters
+set listchars=tab:\|\ ,trail:·,extends:»,precedes:«
+set tabstop=2 shiftwidth=2      " a tab is two spaces
+" searching
+set gdefault                    " g flag by default for :s
+set matchpairs+=<:>             " add < and > to list of "matching" brackets
+set showmatch                   " flash matching bracket
+set ignorecase                  " searches are case insensitive...
+set smartcase                   " ... unless they contain at least one capital letter
+" ui
+set guifont=FiraCode\ Nerd\ Font\ Mono\ Retina:h15
+set cursorline                  " highlight current line
+set scrolloff=14                " leave this amount of lines while scrolling up/down
+" behavioral
+let mapleader = "\<space>"      " set the trigger key for plugins/shortcuts
+set autowrite                   " auto-write file before certain operations
+set clipboard+=unnamedplus      " always use clipboard for yank/paste
+set completeopt+=noinsert,menuone " completion menu options
+set formatoptions+=21           " use indent from second line of a paragraph, don't break line after single letter word
+set keymodel+=startsel          " shift+special key = selection
+set nowritebackup               " no backup files, even while vim is running
+set number                      " display current line number
+set shortmess-=l                " verbose "x lines, y characters" in messages
+set shortmess+=mrwI             " shorter indicators (like [RO]) in messages, no :intro on start
+set splitbelow splitright       " change splits placement
+set ssop+=winpos,localoptions   " save window positions and local options for :mksession
+set undofile                    " write undo history to undo files
+]])
+
+
+-- autocommands
+vim.cmd([[
+" remove trailing whitespace on save
+autocmd BufWritePre * :%s/\s\+$//e
+" open folds in newly opened files
+autocmd BufRead,BufNewFile * normal zR
+]])
+
+
+-- abbrevations
+vim.cmd([[
+iab --- —
+iab --> →
+iab <-- ←
+iab <expr> dts strftime('%Y-%m-%d')
+]])
+
+
+-- keyboard mappings
+vim.cmd([[
+" disable arrow keys
+map <up> <nop>
+map <down> <nop>
+map <left> <nop>
+map <right> <nop>
+imap <up> <nop>
+imap <down> <nop>
+imap <left> <nop>
+imap <right> <nop>
+nnoremap j gj
+nnoremap k gk
+" moving whole lines
+nmap <C-k> ddkP
+nmap <C-j> ddp
+vmap <C-k> xkP`[V`]
+vmap <C-j> xp`[V`]
+" better tab
+noremap <tab> v>
+noremap <s-tab> v<
+vnoremap <tab> >gv
+vnoremap <s-tab> <gv
+" window operations
+nmap <C-C> :close<cr>
+" easier escape
+inoremap jj <ESC>
+" forgot your sudo?
+cmap w!! w !sudo tee % >/dev/null
+" no yank on paste
+xnoremap p pgvy
+" don't move the cursor on *
+nnoremap <silent> * :let stay_star_view = winsaveview()<cr>*:call winrestview(stay_star_view)<cr>
+" keep search matches in the middle of the window
+nnoremap n nzzzv
+nnoremap N Nzzzv
+" cd to current file's path
+nnoremap <leader>cd :cd %:p:h<CR>:pwd<CR>
+" edit the setting file
+nmap <silent> <leader>rc :n $MYVIMRC<CR>
+]])
+
+
+-- Plugins via lazy.nvim
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -11,8 +106,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-
--- install plugins via lazy
 require('lazy').setup({
   {'ellisonleao/gruvbox.nvim', priority = 1000 , config = true},
   'hiphish/rainbow-delimiters.nvim',
@@ -48,26 +141,6 @@ require('lazy').setup({
 })
 -- activate plugins
 require('lualine').setup()
-
-
--- nvim options
---
--- whitespace
-vim.opt.expandtab = true
-vim.opt.list = true
-vim.opt.listchars = {tab = '| ', trail = '·', extends = '»', precedes = '«'}
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
--- ui
-vim.opt.clipboard:append('unnamedplus')
-vim.opt.cursorline = true
-vim.opt.guifont = 'FiraCode Nerd Font Mono Retina:h15'
--- searching
-vim.opt.gdefault = true
-vim.opt.ignorecase = true
-vim.opt.matchpairs:append('<:>')
-vim.opt.showmatch = true
-vim.opt.smartcase = true
 
 
 -- colors
