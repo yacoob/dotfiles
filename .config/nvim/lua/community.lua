4bb9bfbaede81ce648a37007ba764afda849f4c1
Jakub Turski <yacoob@ftml.net>
2024-06-23 17:30:56 +0100

    (nvim) Add neogit and diffview.nvim.

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -1,5 +1,3 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -9,5 +7,6 @@ return {
   'AstroNvim/astrocommunity',
   {import = 'astrocommunity.recipes.telescope-lsp-mappings'},
   {import = 'astrocommunity.pack.markdown'},
-  -- import/override with your plugins folder
+  {import = 'astrocommunity.git.neogit'},
+  {import = 'astrocommunity.git.diffview-nvim'},
 }
