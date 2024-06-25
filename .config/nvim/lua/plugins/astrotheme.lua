390c810faf07d8d80c639c6b4d9e7736c501ef00
Jakub Turski <yacoob@ftml.net>
2024-06-25 16:30:59 +0100

    (vim) Better highlight for found strings.

---
+++ .config/nvim/lua/plugins/astrotheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "AstroNvim/astrotheme",
+    ---@type AstroUIOpts
+    opts = {
+      highlights = {
+        astrodark = {
+          -- make phrases found by / stand out more
+          Search = {
+            underline = true,
+            fg = '#1a1d23',
+            bg = '#50a4e9',
+          },
+        },
+      },
+    },
+  },
+}
