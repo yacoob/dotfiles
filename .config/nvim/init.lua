874c4537c9a4be8115f63c6b3acaa65b277dcc46
Jakub Turski <yacoob@ftml.net>
2024-06-25 18:05:36 +0100

    (nvim) streamline init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,38 +2,26 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   -- stylua: ignore
-  vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath })
+  vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable',
+    lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   {
     'AstroNvim/AstroNvim',
-    version = '^4', -- Remove version tracking to elect for nighly AstroNvim
+    version = '^4',
     import = 'astronvim.plugins',
-    opts = { -- astronvim options must be set here with the `import` key
-      mapleader = ' ', -- this ensures the leader key must be configured before lazy is set up
-      maplocalleader = ',', -- This ensures the localleader key must be configured before Lazy is set up
-      icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
-      pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
-      update_notifications = true, -- Enable/disable notification about running `:Lazy update` twice to update pinned plugins
+    opts = {
+      mapleader = ' ',
+      maplocalleader = ',',
+      icons_enabled = true,
+      pin_plugins = nil,
+      update_notifications = true,
     },
   },
-  { import = 'community' },
-  { import = 'plugins' },
+  { import = 'community' }, -- add AstroNvim Community packs https://github.com/AstroNvim/astrocommunity
+  { import = 'plugins' },   -- load local plugin configs
 } --[[@as LazySpec]], {
-  -- Configure any other `lazy.nvim` configuration options here
-  install = { colorscheme = { 'astrodark', 'habamax' } },
+  install = { colorscheme = { 'astrodark' } },
   ui = { backdrop = 100 },
-  performance = {
-    rtp = {
-      -- disable some rtp plugins, add more to your liking
-      disabled_plugins = {
-        'gzip',
-        'netrwPlugin',
-        'tarPlugin',
-        'tohtml',
-        'zipPlugin',
-      },
-    },
-  },
 } --[[@as LazyConfig]])
