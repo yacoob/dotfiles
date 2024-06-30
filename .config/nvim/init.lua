17d410a8deaab310479b8e6786e6b6a144c71b0d
Jakub Turski <yacoob@ftml.net>
2024-06-30 21:09:21 +0100

    (nvim) Add a way to configure plugins per host.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,13 @@
--- bootstrap AstroNvim via Lazy
+-- install Lazy if it's not there
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   -- stylua: ignore
   vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable',
     lazypath })
 end
-vim.opt.rtp:prepend(lazypath)
-require('lazy').setup({
+
+-- Add AstroNvim to plugin spec
+local spec = {
   {
     'AstroNvim/AstroNvim',
     version = '^4',
@@ -19,8 +20,20 @@ require('lazy').setup({
       update_notifications = true,
     },
   },
-  { import = 'plugins' }, -- load local plugin configs
-} --[[@as LazySpec]], {
+  { import = 'plugins' },
+} --[[@as LazySpec]]
+
+-- add a host-specific config if it exists
+local hostname = vim.fn.hostname()
+local hostconfig = vim.fn.stdpath 'config' .. '/lua/host/' .. hostname .. '.lua'
+if (vim.uv or vim.loop).fs_stat(hostconfig) then
+  table.insert(spec, { import = 'host.' .. hostname})
+end
+
+-- run Lazy
+vim.opt.rtp:prepend(lazypath)
+require('lazy').setup(spec,
+{
   install = { colorscheme = { 'astrodark' } },
   ui = { backdrop = 100 },
 } --[[@as LazyConfig]])
