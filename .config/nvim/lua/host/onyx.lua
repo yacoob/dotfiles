d45f8c7c2ac5c473413157248e2b467237857608
Jakub Turski <yacoob@ftml.net>
2024-06-30 21:16:59 +0100

    (nvim) Add all language packs that I routinely use on my desktop.

--- .config/nvim/lua/host/onyx.lua
+++ .config/nvim/lua/host/onyx.lua
@@ -1,6 +1,10 @@
 return {
   {
     'AstroNvim/astrocommunity',
+    { import = 'astrocommunity.pack.docker' },
+    { import = 'astrocommunity.pack.json' },
     { import = 'astrocommunity.pack.python-ruff' },
+    { import = 'astrocommunity.pack.toml' },
+    { import = 'astrocommunity.pack.yaml' },
   },
 }
