aaf2a2a3b237c6cf20904a7e3b5c1f0a5ec1d3a2
Jakub Turski <yacoob@ftml.net>
2024-06-30 19:42:28 +0100

    (nvim) enable flash
    https://github.com/folke/flash.nvim

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -9,6 +9,7 @@ return {
   { import = 'astrocommunity.editing-support.wildfire-nvim' },
   { import = 'astrocommunity.git.neogit' },
   { import = 'astrocommunity.git.diffview-nvim' },
+  { import = 'astrocommunity.motion.flash-nvim' },
   { import = 'astrocommunity.pack.markdown' },
   { import = 'astrocommunity.pack.python-ruff' },
   { import = 'astrocommunity.recipes.telescope-lsp-mappings' },
