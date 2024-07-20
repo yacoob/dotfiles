816364d4f123338cdae44714709c519b9725b569
Jakub Turski <yacoob@ftml.net>
2024-07-20 22:08:17 +0100

    (nvim) explicitly ask for isorting before saving a python file

--- .config/nvim/lua/plugins/astrocore.lua
+++ .config/nvim/lua/plugins/astrocore.lua
@@ -18,7 +18,22 @@ return {
               vim.b[args.buf].large_buf = false
               require('astrocore').event('LargeBuf', true)
             end
-          }
+          },
+          -- ruff is really good, but so far it still can't sort imports before file save
+          -- astrocommunity.pack.python-ruff explicitly removes isort from ensure_installed
+          -- my options were either an autocommand or bringing back isort
+          -- I went with an autocommand, to make sure it's ruff doing the isorting, so if I
+          -- need to configure things, I don't need a separate cfg file for isort
+          -- https://github.com/astral-sh/ruff-lsp/issues/95
+          {
+            event = { 'BufWritePre' },
+            pattern = { '*.py' },
+            desc = 'Sort python imports',
+            callback =  function()
+              vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true, })
+              vim.wait(100)
+            end
+          },
         },
       },
       options = {
