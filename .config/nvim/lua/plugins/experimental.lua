9e8b4dbacc2d452f8d6a7b818cad8d1c99e8c6bf
Jakub Turski <yacoob@ftml.net>
2024-07-30 18:22:24 +0100

    (nvim) make the window borders more prominent

--- .config/nvim/lua/plugins/experimental.lua
+++ .config/nvim/lua/plugins/experimental.lua
@@ -4,5 +4,6 @@ return {
   { import = 'astrocommunity.completion.copilot-cmp' },
   { import = 'astrocommunity.search.grug-far-nvim' },
   { import = 'astrocommunity.utility.noice-nvim' },
+  { import = 'astrocommunity.split-and-window.colorful-winsep-nvim' },
   -- { import = 'astrocommunity. ...' },
 }
