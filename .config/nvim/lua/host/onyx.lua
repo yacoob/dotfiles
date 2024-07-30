12703f483773461af9ee8f0a28d9c0c05c214249
Jakub Turski <yacoob@ftml.net>
2024-07-30 16:10:57 +0100

    (nvim) add overseer, neotest and coverage on dev machines

--- .config/nvim/lua/host/onyx.lua
+++ .config/nvim/lua/host/onyx.lua
@@ -1,11 +1,14 @@
 return {
   {
     'AstroNvim/astrocommunity',
-    { import = 'astrocommunity.editing-support/nvim-devdocs'},
+    { import = 'astrocommunity.code-runner.overseer-nvim' },
+    { import = 'astrocommunity.editing-support.nvim-devdocs' },
     { import = 'astrocommunity.pack.docker' },
     { import = 'astrocommunity.pack.json' },
     { import = 'astrocommunity.pack.python-ruff' },
     { import = 'astrocommunity.pack.toml' },
     { import = 'astrocommunity.pack.yaml' },
+    { import = 'astrocommunity.test.neotest' },
+    { import = 'astrocommunity.test.nvim-coverage' },
   },
 }
