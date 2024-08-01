d5b0902ceb49e9378e164e7bbed70273f7e4c623
Jakub Turski <yacoob@ftml.net>
2024-08-01 21:16:03 +0100

    (nvim) disable noice, use lsp's function signature tooltip

--- .config/nvim/lua/plugins/experimental.lua
+++ .config/nvim/lua/plugins/experimental.lua
@@ -3,7 +3,7 @@ return {
   'AstroNvim/astrocommunity',
   { import = 'astrocommunity.completion.copilot-cmp' },
   { import = 'astrocommunity.search.grug-far-nvim' },
-  { import = 'astrocommunity.utility.noice-nvim' },
+  -- { import = 'astrocommunity.utility.noice-nvim' },
   { import = 'astrocommunity.split-and-window.colorful-winsep-nvim' },
   -- { import = 'astrocommunity. ...' },
 }
