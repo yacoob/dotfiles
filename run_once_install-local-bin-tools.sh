bef7af72e679b9dbaba6adc0c90170bf6b827af5
Jakub Turski <yacoob@ftml.net>
2024-08-07 00:54:01 +0100

    chezmoi: first take at tools installation

---
+++ run_once_install-local-bin-tools.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eu
+cd ~/.local/bin
+curl https://zyedidia.github.io/eget.sh | sh
+eg=~/.local/bin/eget
+${eg} BurntSushi/ripgrep
+${eg} ClementTsang/bottom -a gnu.tar -f btm
+${eg} jesseduffield/lazygit
+${eg} junegunn/fzf -a amd64.tar
+${eg} neovim/neovim -a appimage
+${eg} newren/git-filter-repo
+${eg} sharkdp/bat -a gnu.tar
+${eg} sharkdp/fd -a gnu.tar
+${eg} starship/starship -a gnu.tar
+${eg} watchexec/watchexec -a gnu.tar -a '^b3' -a '^sha512'
+${eg} wfxr/forgit -f bin/git-forgit
+${eg} Wilfred/difftastic
