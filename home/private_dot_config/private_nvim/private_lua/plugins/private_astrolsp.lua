8d4a68ccc4eaa6c7a2582eddeca03852d7c3da85
Jakub Turski <yacoob@ftml.net>
2024-11-14 01:37:47 +0000

    nvim: tell dockerls not to format multiline commands
    Also s/TAB/space/

--- home/private_dot_config/private_nvim/private_lua/plugins/private_astrolsp.lua
+++ home/private_dot_config/private_nvim/private_lua/plugins/private_astrolsp.lua
@@ -4,29 +4,40 @@ local rp = require("lspconfig.util").root_pattern
 -- right dir for monorepos, where you have multiple pyproject.toml files in
 -- different subdirs and one virtualenv for them in an ascendant dir.
 local pyright_root_files =
-	{ ".venv", "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", "pyrightconfig.json", ".git" }
+  { ".venv", "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", "pyrightconfig.json", ".git" }
 local ruff_root_files = { ".venv", "pyproject.toml", "ruff.toml", ".ruff.toml" }
 
 return {
-	"AstroNvim/astrolsp",
-	---@type AstroLSPOpts
-	opts = {
-		features = {
-			signature_help = true,
-			inlay_hints = true,
-		},
-		---@diagnostic disable-next-line: missing-fields
-		config = {
-			basedpyright = {
-				root_dir = function(fname)
-					return rp(unpack(pyright_root_files))(fname)
-				end,
-			},
-			ruff = {
-				root_dir = function(fname)
-					return rp(unpack(ruff_root_files))(fname)
-				end,
-			},
-		},
-	},
+  "AstroNvim/astrolsp",
+  ---@type AstroLSPOpts
+  opts = {
+    features = {
+      signature_help = true,
+      inlay_hints = true,
+    },
+    ---@diagnostic disable-next-line: missing-fields
+    config = {
+      basedpyright = {
+        root_dir = function(fname)
+          return rp(unpack(pyright_root_files))(fname)
+        end,
+      },
+      dockerls = {
+        settings = {
+          docker = {
+            languageserver = {
+              formatter = {
+                ignoreMultilineInstructions = true
+              },
+            },
+          },
+        },
+      },
+      ruff = {
+        root_dir = function(fname)
+          return rp(unpack(ruff_root_files))(fname)
+        end,
+      },
+    },
+  },
 }
