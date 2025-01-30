bbfaff7639e697ca7447b3c55b491047936ae12e
Jakub Turski <yacoob@ftml.net>
2025-01-30 20:03:00 +0000

    nvim: allow for explicit specification of astrocommunity pack

--- home/dot_config/nvim/lua/plugins/devpod.lua
+++ home/dot_config/nvim/lua/plugins/devpod.lua
@@ -1,27 +1,26 @@
 local ac = {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.code-runner.overseer-nvim" },
-  { import = "astrocommunity.editing-support.refactoring-nvim" },
-  { import = "astrocommunity.pack.docker" },
-  { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.toml" },
-  { import = "astrocommunity.pack.yaml" },
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.code-runner.overseer-nvim" },
+	{ import = "astrocommunity.editing-support.refactoring-nvim" },
+	{ import = "astrocommunity.pack.docker" },
+	{ import = "astrocommunity.pack.json" },
+	{ import = "astrocommunity.pack.toml" },
+	{ import = "astrocommunity.pack.yaml" },
 }
 
 if os.getenv("DEVPOD") then
-  vim.list_extend(ac, {
-    { import = "astrocommunity.editing-support.nvim-devdocs" },
-    { import = "astrocommunity.test.neotest" },
-    { import = "astrocommunity.test.nvim-coverage" },
-  })
+	vim.list_extend(ac, {
+		{ import = "astrocommunity.editing-support.nvim-devdocs" },
+		{ import = "astrocommunity.test.neotest" },
+		{ import = "astrocommunity.test.nvim-coverage" },
+	})
 
-  local flavour = os.getenv("DEVPOD_FLAVOUR")
-
-  if flavour == "python" then
-    vim.list_extend(ac, {
-      { import = "astrocommunity.pack.python-ruff" },
-    })
-  end
+	local pack = os.getenv("DEVPOD_NVIM_ASTROCOMMUNITY_PACK")
+	if pack and string.match(pack, "^[A-Za-z-]+$") then
+		vim.list_extend(ac, {
+			{ import = "astrocommunity.pack." .. pack },
+		})
+	end
 end
 
 return ac
