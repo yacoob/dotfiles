f30490c32bc4e979f6b7d32317ec2c0cd12e6219
Jakub Turski <yacoob@ftml.net>
2025-12-18 23:55:07 +0000

    nvim: let's try astronvim v6

---
+++ home/dot_config/nvim/lua/plugins/astronvim-v6.lua
@@ -0,0 +1,5 @@
+return {
+  { "AstroNvim/astrocore", version = false, branch = "v3" },
+  { "AstroNvim/astrolsp", version = false, branch = "v4" },
+  { "AstroNvim/astroui", version = false, branch = "v4" },
+}
