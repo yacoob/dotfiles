6c97d60642f72fc1565d1c904231e40ec36b55e0
Jakub Turski <yacoob@ftml.net>
2026-03-10 23:58:56 +0000

    fish: make a distinction between abbreviations and aliases
    abbreviations are expanded in-place in "command positions" as I type

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -36,16 +36,18 @@ status is-interactive || exit 0
 set -g fish_greeting
 set -g fish_key_bindings fish_vi_key_bindings
 
+# abbreviations
+abbr --add cp 'cp -i'
+abbr --add mv 'mv -i'
+abbr --add rm 'rm -i'
+abbr --add grep 'grep -E'
+abbr --add la 'ls -A'
+abbr --add ll 'ls -lh'
+abbr --add lla 'ls -lhA'
+abbr --add k kubectl
+abbr --add refish 'exec fish'
+
 # aliases
-#
-alias cp 'cp -i'
-alias mv 'mv -i'
-alias rm 'rm -i'
-alias grep 'egrep --color=auto'
-alias ll 'ls -lh'
-alias la 'ls -A'
-alias lla 'll -A'
-alias k kubectl
 alias apt-list-backports 'aptitude search -t (lsb_release -sc)-backports -F "%p %v -> %V" "~U ~Abackports"'
 alias beets-container 'podman compose -f ~/.local/bin/beets/compose.yaml pull --policy=always && podman compose -f ~/.local/bin/beets/compose.yaml run --rm beets'
 
@@ -109,8 +111,8 @@ end
 
 # kubert
 if command -q kubert
-  alias kctx 'kubert ctx'
-  alias kns 'kubert ns'
+  abbr --add kctx 'kubert ctx'
+  abbr --add kns 'kubert ns'
 end
 
 # vi/vim/nvim
