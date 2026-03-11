733bbfce920071e312980bb2c66851fe2440fef1
Jakub Turski <yacoob@ftml.net>
2026-03-11 23:46:14 +0000

    fish: ensure hooks runs as late as possible
    It contains optional atuin activation. I want atuin's ctrl-r handler to
    win over fzf.fish's one.
    
    Also, rebind process chooser to ctrl-alt-i, as ctrl-alt-p is taken by
    the IME.

--- home/dot_config/fish/conf.d/zz-hooks.fish
+++ home/dot_config/fish/conf.d/zz-hooks.fish
@@ -2,18 +2,28 @@
 #
 status is-interactive || exit 0
 
+# fzf.fish keybindings
+if functions -q fzf_configure_bindings
+    fzf_configure_bindings --processes=ctrl-alt-i
+end
+
+# atuin - will override ctrl-r bind
 if command -q atuin
     atuin init fish --disable-up-arrow | source
 end
 
+# mise chdir hooks
 if command -q mise
     mise activate fish | source
 end
 
+# prompt via starship
 if command -q starship
     starship init fish | source
 end
 
+# zoxide chdir hooks
 if command -q zoxide
     zoxide init fish | source
 end
+
