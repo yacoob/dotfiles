e74f32ffca5221dc90f6150a5cb57389f3f59747
Jakub Turski <yacoob@ftml.net>
2026-04-10 12:34:37 +0100

    fish: activate mise early to make sure the binaries it provides are available in PATH

--- home/dot_config/fish/conf.d/02-extra-binaries.fish
+++ home/dot_config/fish/conf.d/02-extra-binaries.fish
@@ -1,15 +1,18 @@
 # homebrew
-#
-# always modify PATH etc.
 if not set -q HOMEBREW_PREFIX
     if test -x /home/linuxbrew/.linuxbrew/bin/brew
         eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
     end
 end
 
+# mise
+if command -q mise
+    mise activate fish | source
+end
+
 status is-interactive || exit 0
 
-# ...but only add completions in an interactive shell
+# only add homebrew completions in an interactive shell
 if set -q HOMEBREW_PREFIX
     for dir in $HOMEBREW_PREFIX/share/fish/{completions,vendor_completions.d}
         if test -d $dir
